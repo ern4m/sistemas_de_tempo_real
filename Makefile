8b1ef705fa693ed74e8566a752be38edd7ad16d9
ern4m <ernam.rsf@gmail.com>
2023-09-29 10:57:52 +0000

    16 threads 4x4 matrix multiplication

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: SomaThreads  SomaThreadsSemaforo
+all: SomaThreads  SomaThreadsSemaforo  MatMul
 
 SomaThreads: SomaThreads.c
 	gcc -o ./out/SomaThreads SomaThreads.c
 
 SomaThreadsSemaforo: SomaThreadsSemaforo.c
 	gcc -o ./out/SomaThreadsSemaforo SomaThreadsSemaforo.c
+
+MatMul: MatMul.c
+	gcc -o ./out/MatMul MatMul.c
---
+++ MatMul.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAT4_SIZE 4
+
+// Define um struct para uma mat4
+struct mat4
+{
+    int data[MAT4_SIZE][MAT4_SIZE];
+};
+
+struct ThreadData
+{
+    int row;
+    int col;
+};
+
+// Inicialização do array de semaforos 
+sem_t mutex[MAT4_SIZE][MAT4_SIZE];
+
+// Criando as matrizes
+struct mat4 matA;
+struct mat4 matB;
+struct mat4 matRES;
+
+
+void* matMul(void* arg)
+{
+    struct ThreadData* data = (struct ThreadData*) arg;
+    int row = data->row;
+    int col = data->col;
+
+    int tmp = 0;
+    for (int k = 0; k < MAT4_SIZE; k++) {
+        tmp += matA.data[row][k] * matB.data[k][col];
+    }
+    
+    sem_wait(&mutex[row][col]);
+    matRES.datarow][col] = tmp;
+    sem_post(&mutex[row][col]);
+
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    // Inicializando matA e matB
+    for (int i = 0; i < MAT4_SIZE; i++) {
+        for (int j = 0; j < MAT4_SIZE; j++) {
+            // Atribuição de valores
+            matA.data[i][j] = i * MAT4_SIZE + j + 1;
+            matB.data[i][j] = i * MAT4_SIZE + j + 1;
+        }
+    }
+
+    // Criando estrutura de threads
+    pthread_t threads[MAT4_SIZE][MAT4_SIZE];
+    struct ThreadData threadData[MAT4_SIZE][MAT4_SIZE];
+
+
+    // Inicializando os semaforos
+    for (int i = 0; i < MAT4_SIZE; i++) {
+        for (int j = 0; j < MAT4_SIZE; j++) {
+            // Estado inicial de todos como disponível
+            sem_init(&mutex[i][j], 0, 1);
+        }
+    }
+
+    // Criar as threads
+    for (int i = 0; i < MAT4_SIZE; i++) {
+        for (int j = 0; j < MAT4_SIZE; j++) {
+            // Criando os argumentos para a ThreadData
+            // Será a posição de cada row x col que serao multiplicadas
+            threadData[i][j].row = i;
+            threadData[i][j].col = j;
+
+            // Criando a thread
+            pthread_create(&threads[i][j], NULL, matMul, &threadData[i][j]);
+        }
+    }
+
+    // Esperar o fim das threads
+    for (int i = 0; i < MAT4_SIZE; i++) {
+        for (int j = 0; j < MAT4_SIZE; j++) {
+            // liberando as threads
+            pthread_join(threads[i][j], NULL);
+        }
+    }
+
+    // Printar as matrizes
+    printf("4x4 Matrix:\n");
+    for (int i = 0; i < MAT4_SIZE; i++) {
+        for (int j = 0; j < MAT4_SIZE; j++) {
+            printf("%d\t", matRES.data[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Destruir os semaforos
+    for (int i = 0; i < MAT4_SIZE; i++) {
+        for (int j = 0; j < MAT4_SIZE; j++) {
+            sem_destroy(&mutex[i][j]);
+        }
+    }
+
+    return 0;
+}
