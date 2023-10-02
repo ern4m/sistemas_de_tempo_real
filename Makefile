4f6aaa438ec432d6e263db579c290c808f686101
ern4m <ernam.rsf@gmail.com>
2023-10-01 21:39:19 -0300

    Finished Search with threads and semaphores

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: SomaThreads  SomaThreadsSemaforo  MatMul
+all: SomaThreads  SomaThreadsSemaforo  MatMul Search
 
 SomaThreads: SomaThreads.c
 	gcc -o ./out/SomaThreads SomaThreads.c
@@ -8,3 +8,6 @@ SomaThreadsSemaforo: SomaThreadsSemaforo.c
 
 MatMul: MatMul.c
 	gcc -o ./out/MatMul MatMul.c
+
+search: Search.c
+	gcc -o ./out/Search Search.c
--- Search.c
+++ Search.c
@@ -1,83 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <threads.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdbool.h>
 
-#define VECSIZE 10
-#define NUM_THREAD 4
+#define ARRAY_SIZE 100
+#define NUM_THREADS 4
 
-struct Result
-{
-    bool found;
-    int idx;
-};
+int target = 42; // Number to be searched
+int found = 0;   // Tmp to check if value was found
+int array[ARRAY_SIZE];
+sem_t found_sem;
+sem_t print_sem;
 
-struct ThreadData
-{
-    int idx, length;
-};
+// Function that each thread will execute to search for the target number
+void* search(void* arg) {
+    int thread_id = *(int*)arg;
+    int chunk_size = ARRAY_SIZE / NUM_THREADS;
+    int start = thread_id * chunk_size;
+    // verify if it's the last thread, if true the end of the search will be ARRAY SIZE
+    // else it will be the start + values per thread
+    int end = (thread_id == (NUM_THREADS - 1)) ? ARRAY_SIZE : start + chunk_size;
 
-int vec[VECSIZE];
-
-int target = 3;
-
-struct Result res = {false, -1};
-
-sem_t mutex[VECSIZE];
-
-void* findNum(void* arg)
-{
-    struct ThreadData* data = (struct ThreadData*) arg;
-    
-    int curr;
-
-    for (int i = data->idx; i < data->length; i++) {
-        curr = vec[i];
-        if (curr == target) {
-            res.found = true;
-            res.idx = i;
-            printf("Found number: %d, at index: %d", curr, i);
+    for (int i = start; i < end; i++) {
+        if (array[i] == target) {
+            sem_wait(&found_sem); // Wait for permission to update found
+            if (!found) {
+                found = 1; // Set found flag if not already set
+            }
+            sem_post(&found_sem); // Release the semaphore
+            break;
         }
     }
 
+    sem_post(&print_sem); // Signal that this thread has finished searching
     pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[])
-{
-    int length, remainder;
+int main() {
+    pthread_t threads[NUM_THREADS];
+    int thread_ids[NUM_THREADS];
+
+    // Initialize the array with random values
     srand(time(NULL));
-
-    length = VECSIZE / NUM_THREAD; // Tamanho dos dados de cada thread (divisão inteira)
-	remainder = VECSIZE % NUM_THREAD; // Resto da divisão inteira
-
-    // Populating the 100 sized array with rand numbers from up to 1000
-    for (int i = 0; i < VECSIZE; i++) {
-        int num_rand = rand() % 1000 + 1;
-        vec[i] = num_rand;
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = rand() % 100;
     }
-         
-    printf("[");
-    for (int i = 0; i < VECSIZE; i++) {
-        printf("%d", vec[i]);
-        if (i != VECSIZE-1) {
-            printf(",");
-        }
+
+    // print array
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        int a = array[i];
+        printf("%d - ", a);
     }
-    printf("]");
+    sem_init(&found_sem, 0, 1); // Initialize the found semaphore
+    sem_init(&print_sem, 0, 0); // Initialize the print semaphore
 
-    pthread_t threads[NUM_THREAD];
-    struct ThreadData threadData[NUM_THREAD];
+    // Create and launch threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, search, &thread_ids[i]);
+    }
 
-	for(int i = 0; i < NUM_THREAD; i++) {
-		threadData[i].idx = i * length;
-		threadData[i].length = length;
-		if(i == (NUM_THREAD - 1)) { // Testa se é ultima thread
-			threadData[i].length += remainder; // A última recebe o resto, além dos dados
-        }
-        pthread_create(&threads[i], NULL, findNum, &threadData[i]);
-	}
+    // Wait for all threads to finish searching
+    for (int i = 0; i < NUM_THREADS; i++) {
+        sem_wait(&print_sem); // Wait for permission to print
+    }
+
+    // Check if the number was found
+    if (found) {
+        printf("Number %d was found.\n", target);
+    } else {
+        printf("Number %d was not found.\n", target);
+    }
+
+    // Clean up semaphores
+    sem_destroy(&found_sem);
+    sem_destroy(&print_sem);
+
+    return 0;
 }
